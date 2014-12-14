ae3e5a00d413c3d9cd1880052284ec3806639138
TerryHung <B00201015@gmail.com>
2014-12-14 23:00:58 +0800

    Remove final

--- hw5/p17.py
+++ hw5/p17.py
@@ -7,11 +7,14 @@ if __name__ == "__main__":
     filename = "features.train"
     X, y = read_data_file(filename)
     X = np.asarray(X, dtype="float64")
+    y = np.array(y)
 
     for i in [0, 2, 4, 6, 8]:
-        y_0 = map(lambda x: 0 if x == i else float(1), y)
+        # y_0 = map(lambda x: 0 if x == i else float(1), y)
+        y_0 = (y == i).astype(int)
         y_0 = np.asarray(y_0, dtype="float64")
         res = svm.libsvm.fit(X, y_0, C=0.01, kernel="poly", degree=2)
+        import ipdb; ipdb.set_trace()
         sum_a = np.abs(res[3]).sum()
 
         print sum_a
