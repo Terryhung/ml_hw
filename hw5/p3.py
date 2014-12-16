d08c985698502464dbae9bc98412fa14839a321a
TerryHung <B00201015@gmail.com>
2014-12-16 17:37:37 +0800

    Fix bug in p3

--- hw5/p3.py
+++ hw5/p3.py
@@ -1,20 +1,11 @@
-from cvxopt import matrix, solvers
+from sklearn import svm
 import numpy as np
-X = [[-1, -1, 0], [-1, 0, -1], [-1, 0, 1], [1, -1, 0], [1, 0, 2], [1, 0, -2], [1, -2, 0]]
+X = [[1, 1, 0], [1, 0, 1], [1, 0, -1], [1, -1, 0], [1, 0, 2], [1, 0, -2], [1, -2, 0]]
 Y = [-1, -1, -1, 1, 1, 1, 1]
 
-Q = matrix([[0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1, 0, 0, 0, 0, 0, 0],
-     [0, 0, 1, 0, 0, 0, 0, 0],
-     [0, 0, 0, 1, 0, 0, 0, 0],
-     [0, 0, 0, 0, 1, 0, 0, 0],
-     [0, 0, 0, 0, 0, 1, 0, 0],
-     [0, 0, 0, 0, 0, 0, 1, 0],
-     [0, 0, 0, 0, 0, 0, 0, 1]])
+X = np.asarray(X, dtype="float64")
+Y = np.asarray(Y, dtype="float64")
 
-q = matrix([0, 0, 0, 0, 0, 0, 0, 0])
+clf = svm.libsvm.fit(X, Y, kernel="poly", degree=2 , C=10000000)
 
-A = np.array(X)
-c = 1
-
-sol=solvers.qp(Q, q, matrix(A), c)
+print clf[1]
